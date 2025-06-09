3d58a6ab6fc5565238405611e888bde024f628f9
kaimu-r <kaimu.seino@rizapgroup.com>
2025-06-09 14:20:36 +0900

    #3 resourcesメソッドの代わりに個別でルーティングを定義する場合をコメントで追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,14 @@ Rails.application.routes.draw do
   # ユーザーに関するルーティングを定義
   # 【resources 参考文献】https://api.rubyonrails.org/v8.0/classes/ActionDispatch/Routing/Mapper/Resources.html#method-i-resources
   resources :users
+
+  # resourcesの代わりに個別でルーティングを定義する場合
+  # get 'users', to: 'users#index'
+  # post 'users/create', to: 'users#create'
+  # get 'users/new', to: 'users#new'
+  # get 'users/:id/edit', to: 'users#edit'
+  # get 'users/:id', to: 'users#show'
+  # patch 'users/:id', to: 'users#update'
+  # put 'users/:id', to: 'users#update'
+  # delete 'users/:id', to: 'users#destroy'
 end
