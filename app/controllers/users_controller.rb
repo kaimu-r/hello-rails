c1c02c866d3c490fdaeb7570ff61ae074d9ade4a
kaimu-r <kaimu.seino@rizapgroup.com>
2025-07-03 10:16:17 +0900

    #19 ユーザーの画像を表示するコントローラを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @skills = @user.skills
   end
+
+  # ユーザー画像表示アクション
+  def image
+    user = User.find(params[:id])
+
+    send_data user.image, filename: "user_image", type: "image/png", disposition: "inline"
+  end
 end
