bed985d4c714d78fa1a6150675ae5b3126d6204c
kaimu-r <kaimu.seino@rizapgroup.com>
2025-07-08 13:51:02 +0900

    #23 一般ユーザー画面用の検索フォームとコントローラを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
   # ユーザー一覧ページ
   def index
-    @users = User.order(:full_name).page params[:page]
+    @users = User.all
+    @prefectures = User.prefectures
+
+    # 検索フォームから検索条件のクエリパラメータを取得し、検索を行っていく処理
+    @users = @users.search_by_full_name(params[:name]) if params[:name].present?
+
+    @users = @users.search_by_prefecture(params[:pref]) if @prefectures.value?(params[:pref])
+
+    @users = @users.order_by_birth_date(params[:birth]) if ["asc", "desc"].include?(params[:birth])
+
+    # kaminariのページネーションを追加して、表示件数の制御を行う
+    @users = @users.order(:full_name).page(params[:page])
+
+    @users = @users.per(params[:per]) if ["10", "50", "100"].include?(params[:per])
   end
 
   # ユーザー詳細ページ
