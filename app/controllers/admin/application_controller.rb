ffa89fde618cf2421c5fa1103936746b6acd8686
kaimu-r <kaimu.seino@rizapgroup.com>
2025-07-09 10:59:10 +0900

    #25 ログイン関連のコントローラと共通メソッドを追加

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,3 +1,14 @@
 class Admin::ApplicationController < ActionController::Base
   layout 'admin/application'
+
+  private
+    # ログイン中のユーザーを取得する。
+    def current_admin
+      @_current_admin ||= Admin.find_by(id: session[:admin_id]) if session[:admin_id]
+    end
+
+    # ログインユーザーが存在しない場合はログイン画面にリダイレクトする
+    def login_check
+      redirect_to new_admin_login_path unless current_admin
+    end
 end
