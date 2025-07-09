393c73925dd2f2cca26d0c695b00c0591c98e1e0
kaimu-r <kaimu.seino@rizapgroup.com>
2025-07-09 10:57:57 +0900

    #25 cookieにセッションIDを保持するための設定を追加

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Myapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.session_store :cookie_store, key: '__ssid'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
