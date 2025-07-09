ed68cf13b22f1271ead41a7e39b46d870dda382d
kaimu-r <kaimu.seino@rizapgroup.com>
2025-07-09 13:26:13 +0900

    #23 paramsメソッドをビューテンプレートで使用しないように設定

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,17 +4,23 @@ class Admin::UsersController < Admin::ApplicationController
     @users = User.all
     @prefectures = User.prefectures
 
+    # veiwテンプレートで使用するためインスタンス変数で定義
+    @param_name = params[:name]
+    @param_pref = params[:pref]
+    @param_birth = params[:birth]
+    @param_per = params[:per]
+
     # 検索フォームから検索条件のクエリパラメータを取得し、検索を行っていく処理
-    @users = @users.search_by_full_name(params[:name]) if params[:name].present?
+    @users = @users.search_by_full_name(@param_name) if @param_name.present?
 
-    @users = @users.search_by_prefecture(params[:pref]) if @prefectures.value?(params[:pref])
+    @users = @users.search_by_prefecture(@param_pref) if @prefectures.value?(@param_pref)
 
-    @users = @users.order_by_birth_date(params[:birth]) if ["asc", "desc"].include?(params[:birth])
+    @users = @users.order_by_birth_date(@param_birth) if ["asc", "desc"].include?(@param_birth)
 
     # kaminariのページネーションを追加して、表示件数の制御を行う
     @users = @users.order(:full_name).page(params[:page])
 
-    @users = @users.per(params[:per]) if ["10", "50", "100"].include?(params[:per])
+    @users = @users.per(@param_per) if ["10", "50", "100"].include?(@param_per)
   end
 
   # ユーザー詳細ページ
