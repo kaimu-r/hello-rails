c624f96321fe476a52a2823b3fce11952d98d1e8
kaimu-r <kaimu.seino@rizapgroup.com>
2025-07-11 14:49:04 +0900

    #25 テストをAdminUsersで修正

--- test/controllers/admin/sessions_controller_test.rb
+++ test/controllers/admin/sessions_controller_test.rb
@@ -7,27 +7,27 @@ class Admin::SessionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "#create 管理者がログインできる" do
-    admin = admins(:test_admin)
+    admin_user = admin_users(:test_admin)
 
-    post admin_login_url, params: { email: admin.email }
+    post admin_login_url, params: { email: admin_user.email }
 
     # ログイン後に管理画面の従業員一覧画面にリダイレクトし、セッションのadmin_idと管理者のidが一致していれば良い
     assert_redirected_to admin_users_url
-    assert_equal admin.id, session[:admin_id]
+    assert_equal admin_user.id, session[:admin_user_id]
   end
 
   test "#destroy ログアウトができる" do
-    admin = admins(:test_admin)
+    admin_user = admin_users(:test_admin)
 
     # まずはログインしてセッションを作成
-    post admin_login_url, params: { email: admin.email }
-    assert_equal admin.id, session[:admin_id]
+    post admin_login_url, params: { email: admin_user.email }
+    assert_equal admin_user.id, session[:admin_user_id]
 
     delete admin_login_url
 
     # ログイン画面にリダイレクトし、セッションがないことを確認
     assert_redirected_to new_admin_login_url
-    assert_nil session[:admin_id]
+    assert_nil session[:admin_user_id]
   end
 
   test "アクセスコントロールをテスト（ログインしていないユーザーがAdminページにアクセスできないかをチェック）" do
