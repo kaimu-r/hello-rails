4d6aa9c05b535b0de854dc01628c7d880f7c972c
kaimu-r <kaimu.seino@rizapgroup.com>
2025-07-15 23:55:32 +0900

    #29 AdminUserモデルにOAuthログインのクラスメソッドを追加

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,2 +1,8 @@
 class AdminUser < ApplicationRecord
+  def self.from_auth(access_token)
+    data = access_token.info
+
+    user = where(email: data["email"]).first
+    user = create(email: data["email"], password_digest: "12345678") if user.blank?
+  end
 end
