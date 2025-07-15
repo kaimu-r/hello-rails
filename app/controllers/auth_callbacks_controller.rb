e129af0fa5092bdf8045aa01c925111d1f15a506
kaimu-r <kaimu.seino@rizapgroup.com>
2025-07-15 23:55:54 +0900

    #29 OAuthのCallbackコントローラを追加

---
+++ app/controllers/auth_callbacks_controller.rb
@@ -0,0 +1,15 @@
+class AuthCallbacksController < Admin::ApplicationController
+  skip_before_action :require_login
+
+  def google_oauth2
+    admin_user = AdminUser.from_auth(request.env['omniauth.auth'])
+
+    if admin_user.persisted?
+      reset_session
+      session[:admin_user_id] = admin_user.id
+      redirect_to admin_users_url
+    else
+      redirect_to new_admin_login_path
+    end
+  end
+end
