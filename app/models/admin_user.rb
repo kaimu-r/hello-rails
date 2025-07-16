e1e94ddc0f177cd6bb54b74b94dbeb534e7119de
kaimu-r <kaimu.seino@rizapgroup.com>
2025-07-16 09:58:26 +0900

    #29 `find_or_create_by`メソッドを使用してemailでユーザーが作成されていない場合は、新しく作成して、ユーザーが存在していればadmin_userを返す。

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,8 +1,5 @@
 class AdminUser < ApplicationRecord
   def self.from_auth(access_token)
-    data = access_token.info
-
-    user = where(email: data["email"]).first
-    user = create(email: data["email"], password_digest: "12345678") if user.blank?
+    find_or_create_by(email: access_token.info["email"])
   end
 end
