8f8d8765b0343c0dfacbe155d5e3dbf496c56be0
kaimu-r <kaimu.seino@rizapgroup.com>
2025-07-16 10:08:10 +0900

    #27 パスワードカラムのNULLを許可する

--- db/migrate/20250711060956_add_password_to_admin_users.rb
+++ db/migrate/20250711060956_add_password_to_admin_users.rb
@@ -1,5 +1,5 @@
 class AddPasswordToAdminUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :admin_users, :password_digest, :string, null: false, after: :email
+    add_column :admin_users, :password_digest, :string, null: true, after: :email
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2025_07_11_060956) do
   create_table "admin_users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", null: false
-    t.string "password_digest", null: false
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
