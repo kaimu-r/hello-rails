52406e6922b695729b58f555b348f64e4caa19f8
kaimu-r <kaimu.seino@rizapgroup.com>
2025-07-17 10:19:20 +0900

    #29 メールアドレスが`rizapgroup.com`ではない場合はログインを許可しない

--- app/controllers/admin/auth_callbacks_controller.rb
+++ app/controllers/admin/auth_callbacks_controller.rb
@@ -2,7 +2,12 @@ class Admin::AuthCallbacksController < Admin::ApplicationController
   skip_before_action :require_login
 
   def google_oauth2
-    email = request.env['omniauth.auth'].info["email"]
+    email = request.env["omniauth.auth"].info["email"]
+
+    # メールアドレスが`rizapgroup.com`ではない場合はログインを許可しない。
+    if !email.end_with?("rizapgroup.com")
+      return redirect_to new_admin_login_path
+    end
 
     admin_user = AdminUser.find_or_create_by!(email:)
 
