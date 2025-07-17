2cde895b1ef027572f1d5ee0b29a8634bf0a7f2e
kaimu-r <kaimu.seino@rizapgroup.com>
2025-07-17 09:56:00 +0900

    #29 hdパラメータで`rizapgroup.com`のメールアドレスのみを許可する。

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], hd: 'rizapgroup.com'
 end
 OmniAuth.config.allowed_request_methods = %i[post]
